0d15f7666d9ad542021df48c6c68d2ae5630f2f1
Agne Postnikova <N00211108@iadt.ie>
2025-03-12 18:00:23 +0000

    moved app-sidebar

--- app/itinerary/layout.tsx
+++ app/itinerary/layout.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { useSession } from '@/AuthContext';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
@@ -8,20 +7,13 @@ import { QueryClientProvider } from '@/components/QueryProvider';
 import { ModeToggle } from '@/components/toggle-mode';
 
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/app-sidebar"
+import { AppSidebar } from "@/app/app-sidebar"
 
 
 const AppLayout = ({ children }: Readonly<{
     children: React.ReactNode;
 }>) => {
 
-    // const { user, signOut } = useSession();
-    // const router = useRouter()
-
-    // const handleLogout = async () => {
-    //     await signOut();
-    //     router.replace("/signIn");
-    // };
     return (
         <QueryClientProvider>
             <SidebarProvider>
