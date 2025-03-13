7adcaf94e4327b2b1360f06c0d333e86f7af7232
Agne Postnikova <N00211108@iadt.ie>
2025-03-13 15:00:33 +0000

    fixed env

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,27 +2,17 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from "firebase/auth";
 
 import {getDatabase} from 'firebase/database';
-
+console.log(process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL)
 // Initialize Firebase
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_apiKey,
-//   authDomain: process.env.FIREBASE_authDomain,
-//   databaseURL: process.env.FIREBASE_databaseURL,
-//   projectId: process.env.FIREBASE_projectId,
-//   storageBucket: process.env.FIREBASE_storageBucket,
-//   messagingSenderId: process.env.FIREBASE_messagingSenderId,
-//   appId: process.env.FIREBASE_appId,
-//   measurementId: process.env.FIREBASE_measurementId
-//   };
 const firebaseConfig = {
-  apiKey: "AIzaSyC8wHa8QfrDXT46lkknSLdxGGiW1sp6G0Y",
-  authDomain: "major-project-ap29.firebaseapp.com",
-  databaseURL: "https://major-project-ap29-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "major-project-ap29",
-  storageBucket: "major-project-ap29",
-  messagingSenderId: "290866972249",
-  appId: "1:290866972249:web:ffeaa1308a84956a9f5348",
-  // measurementId: "G-XLSW3PRK51"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,28 @@
 import type { NextConfig } from "next";
 
-import { createRequire } from 'module';
+import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  scope: '/app',
-  sw: 'service-worker.js',
-  disable: process.env.NODE_ENV === 'development',
+const withPWA = require("next-pwa")({
+	dest: "public",
+	register: true,
+	skipWaiting: true,
+	scope: "/app",
+	sw: "service-worker.js",
+	disable: process.env.NODE_ENV === "development"
 });
-
 
 const nextConfig: NextConfig = {
 	/* config options here */
 	env: {
-		apiKey: process.env.FIREBASE_apiKey,
-		authDomain: process.env.FIREBASE_authDomain,
-		databaseURL: process.env.FIREBASE_databaseURL,
-		projectId: process.env.FIREBASE_projectId,
-		storageBucket: process.env.FIREBASE_storageBucket,
-		messagingSenderId: process.env.FIREBASE_messagingSenderId,
-		appId: process.env.FIREBASE_appId,
-		measurementId: process.env.FIREBASE_measurementId
+		apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+		authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+		databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+		projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+		storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+		appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+		measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 	}
 };
 
