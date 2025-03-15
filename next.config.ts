bbe588e21205d1f807a448c36f9c0806172188ea
Agne Postnikova <N00211108@iadt.ie>
2025-03-15 21:11:56 +0000

    working on offline functionality

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
+const firebaseUrlPattern = new RegExp(`^https://${process.env.NEXT_PUBLIC_FB_ID}.europe-west1.firebasedatabase.app/.*`);
 
 const withPWA = require("next-pwa")({
 	dest: "public",
@@ -9,7 +10,33 @@ const withPWA = require("next-pwa")({
 	skipWaiting: true,
 	scope: "/app",
 	sw: "service-worker.js",
-	disable: process.env.NODE_ENV === "development"
+	disable: process.env.NODE_ENV === "development",
+	runtimeCaching: [
+		{
+			// Cache Firebase data for offline use
+			urlPattern: firebaseUrlPattern,
+			handler: "NetworkFirst",
+			options: {
+				cacheName: "firebase-data-cache",
+				expiration: {
+					maxEntries: 50,
+					maxAgeSeconds: 7 * 24 * 60 * 60 // 7 days
+				}
+			}
+		},
+		{
+			// Google Maps resources -  limited offline capabilities
+			urlPattern: /^https:\/\/maps\.googleapis\.com\/.*/,
+			handler: "StaleWhileRevalidate",
+			options: {
+				cacheName: "google-maps-cache",
+				expiration: {
+					maxEntries: 50,
+					maxAgeSeconds: 3 * 24 * 60 * 60 // 1 day
+				}
+			}
+		}
+	]
 });
 
 const nextConfig: NextConfig = {
