8e08e9dafb221cbf570d76d862368a53b0f445dd
Agne Postnikova <N00211108@iadt.ie>
2025-03-27 21:11:34 +0000

    images for the hero

---
+++ utils/landscapeImages.ts
@@ -0,0 +1,22 @@
+import landscape1 from "@/public/landscapes/castle-connemara.jpeg";
+import landscape2 from "@/public/landscapes/cliffs-of-moher.jpeg";
+import landscape3 from "@/public/landscapes/dingle-bridge.jpeg";
+import landscape4 from "@/public/landscapes/giants-causeway.jpeg";
+import landscape5 from "@/public/landscapes/glendalough-lake.jpeg";
+import landscape6 from "@/public/landscapes/kerry-cliffs.webp";
+import landscape7 from "@/public/landscapes/mizen-cork.jpeg";
+import landscape8 from "@/public/landscapes/trees-guinness-lake.jpeg";
+import landscape9 from "@/public/landscapes/wicklow-mtns.jpeg";
+
+// Export as a single array
+export const landscapes = [
+	landscape1,
+	landscape2,
+	landscape3,
+	landscape4,
+	landscape5,
+	landscape6,
+	landscape7,
+	landscape8,
+	landscape9
+];
