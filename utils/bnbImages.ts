3de68fe364ad8e5371481fb1ec1cc98771db32ab
Agne Postnikova <N00211108@iadt.ie>
2025-03-28 21:35:40 +0000

    added images for bnb

---
+++ utils/bnbImages.ts
@@ -0,0 +1,65 @@
+import bnb1 from "@/public/bnbs/image_1.webp";
+import bnb2 from "@/public/bnbs/image_2.jpeg";
+import bnb3 from "@/public/bnbs/image_3.jpeg";
+import bnb4 from "@/public/bnbs/image_4.jpeg";
+import bnb5 from "@/public/bnbs/image_5.jpeg";
+import bnb6 from "@/public/bnbs/image_6.jpeg";
+import bnb7 from "@/public/bnbs/image_7.jpeg";
+import bnb8 from "@/public/bnbs/image_8.jpeg";
+import bnb10 from "@/public/bnbs/image_10.jpeg";
+import bnb11 from "@/public/bnbs/image_11.jpeg";
+import bnb12 from "@/public/bnbs/image_12.jpeg";
+import bnb13 from "@/public/bnbs/image_13.jpeg";
+import bnb14 from "@/public/bnbs/image_14.jpeg";
+import bnb15 from "@/public/bnbs/image_15.jpeg";
+import bnb16 from "@/public/bnbs/image_16.jpeg";
+import bnb17 from "@/public/bnbs/image_17.jpeg";
+import bnb18 from "@/public/bnbs/image_18.jpeg";
+import bnb19 from "@/public/bnbs/image_19.jpeg";
+import bnb20 from "@/public/bnbs/image_20.jpeg";
+import bnb21 from "@/public/bnbs/image_21.jpeg";
+import bnb22 from "@/public/bnbs/image_22.jpeg";
+import bnb23 from "@/public/bnbs/image_23.jpeg";
+import bnb24 from "@/public/bnbs/image_24.jpeg";
+import bnb25 from "@/public/bnbs/image_25.jpeg";
+import bnb26 from "@/public/bnbs/image_26.jpeg";
+import bnb27 from "@/public/bnbs/image_27.jpeg";
+import bnb28 from "@/public/bnbs/image_28.jpeg";
+import bnb29 from "@/public/bnbs/image_29.jpeg";
+import bnb30 from "@/public/bnbs/image_30.jpeg";
+import bnb31 from "@/public/bnbs/image_31.jpeg";
+
+
+// Export as a single array
+export const bnbs = [
+	bnb1,
+	bnb2,
+	bnb3,
+	bnb4,
+	bnb5,
+	bnb6,
+	bnb7,
+	bnb8,
+	bnb10,
+	bnb11,
+	bnb12,
+	bnb13,
+	bnb14,
+	bnb15,
+	bnb16,
+	bnb17,
+	bnb18,
+	bnb19,
+	bnb20,
+	bnb21,
+	bnb22,
+	bnb23,
+	bnb24,
+	bnb25,
+	bnb26,
+	bnb27,
+	bnb28,
+	bnb29,
+	bnb30,
+	bnb31
+];
